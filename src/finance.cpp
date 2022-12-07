9b49cdc9fd1d692abf59587634471de51615d447
awmc000 <mccolmalex@gmail.com>
2022-12-06 18:13:15 -0800

    stub for createBudget()

---
+++ src/finance.cpp
@@ -0,0 +1,12 @@
+//
+// Created by AMC on 12/6/2022.
+//
+#include "../header/finance.h"
+
+namespace finance {
+    Budget createBudget() {
+        Budget new_budget;
+        // todo: implement createBudget();
+        return new_budget;
+    }
+}
